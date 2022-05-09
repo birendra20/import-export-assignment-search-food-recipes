f6ee59f022005253a2f8a8247c6f54ef4ad81e07
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-05-09 16:51:10 +0530

    Add files via upload

---
+++ day-12 import export/components/navbar.js
@@ -0,0 +1,17 @@
+function navbar() {
+  return ` <div id="navbar">
+    <h3><a href="electronics.html">Home</a></h3>
+   
+    
+ 
+    <div id="options">
+      <h3><a href="electronics.html">Latest Food</a></h3>
+      <h3><a href="jewellery.html">Food of The Day</a></h3>
+      <h3><a href="search.html">Search Food</a></h3>
+      <h3><a href="">Sign In</a></h3>
+      <h3><a href="">Sign Up</a></h3>
+    </div>
+  </div>`;
+}
+
+export default navbar;
---
+++ day-12 import export/scripts/showData.js
@@ -0,0 +1,11 @@
+async function getData(url) {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    return data;
+  } catch (e) {
+    console.log("e", e);
+  }
+}
+
+export { getData };
